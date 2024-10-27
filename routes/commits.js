e718e9d0bd544b6ece97890c21c33a62827f27dc
Anubis1711 <semvdvlugt1711@gmail.com>
2024-10-27 14:45:08 +0100

    added last details for demo

--- routes/commits.js
+++ routes/commits.js
@@ -38,5 +38,35 @@ router.get('/', isAuthenticated, async (req, res) => {
   }
 });
 
+router.get('/:repoFullName/:commitSha', isAuthenticated, async (req, res) => {
+  const { repoFullName, commitSha } = req.params;
+
+  try {
+    const accessToken = req.session.accessToken;
+
+    if (!accessToken) {
+      console.error('Geen accessToken gevonden in de sessie');
+      return res.status(401).send('Authenticatie vereist');
+    }
+
+    // API-aanroep naar GitHub om een specifieke commit op te halen
+    const url = `https://api.github.com/repos/${repoFullName}/commits/${commitSha}`;
+
+    const commitResponse = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+
+    // Verstuur de commitgegevens naar de client
+    res.json(commitResponse.data);
+  } catch (error) {
+    console.error("Error tijdens het ophalen van de commit:", error.message);
+    if (error.response) {
+      console.error('Details:', error.response.data);
+    }
+    res.status(500).send("Er ging iets mis bij het ophalen van de commit.");
+  }
+});
 
 module.exports = router;
