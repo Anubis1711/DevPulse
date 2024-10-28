dec3788e3f991976b87e8a7821dd937bf9f7c180
Anubis1711 <semvdvlugt1711@gmail.com>
2024-10-28 13:54:29 +0100

    Ik heb like/ comment functionaliteit toegevoegd

--- models/post.js
+++ models/post.js
@@ -5,7 +5,17 @@ const postSchema = new mongoose.Schema({
   content: { type: String, required: true },
   imageUrl: { type: String },
   commitInfo: { type: String },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  
+  
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+
+
+  comments: [{
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    content: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+  }]
 });
 
 const Post = mongoose.model('Post', postSchema);
